ede34cfa7f670d8b99abc7da406604bb53e45bb0
matheus melo <matheus.melo@avantia.com.br>
2021-02-20 11:04:18 -0300

    full project commit

---
+++ Assets/Scripts/PlayerHeight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHeight : MonoBehaviour
+{
+
+    public Transform player;
+    private Text scoreTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreTxt = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreTxt.text = Mathf.RoundToInt (player.position.y).ToString();
+    }
+}
